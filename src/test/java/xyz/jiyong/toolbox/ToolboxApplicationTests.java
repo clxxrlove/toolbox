a2c25c115dcfc8cc9e8e9bed3c5233876a176408
Jiyong <hickerd@sch.ac.kr>
2024-05-01 01:58:36 +0900

    [Add] Project Initialize

---
+++ src/test/java/xyz/jiyong/toolbox/ToolboxApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.jiyong.toolbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
